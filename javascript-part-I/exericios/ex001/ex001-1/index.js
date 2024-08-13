a041bcbca3a2b8f0aa38eb15e7749ff771d40f3c
GustavoPilar <pilargustavo7@gmail.com>
2024-08-12 21:29:38 -0300

    Exercicios

---
+++ javascript-part-I/exericios/ex001/ex001-1/index.js
@@ -0,0 +1,15 @@
+const primeiroNome = prompt("Qual o seu primerio nome?");
+const sobrenome = prompt("Qual o seu sobrenome?");
+const cursoArea = prompt("Qual o área está cursando?");
+const idade = prompt("Ano de nascimento");
+
+alert (
+    "Recruta cadastrado!\n" +
+    "nome: " + primeiroNome + " " + sobrenome + "\n" +
+    "Curso: " + cursoArea + "\n" +
+    "Idade: " + (2024 - idade)
+);
+
+console.log("Recruta: " + nome);
+console.log("Curso: " + cursoArea);
+console.log("Idade: " + (2024 - idade));
---
+++ javascript-part-I/exericios/ex001/ex001-2/index.js
@@ -0,0 +1,10 @@
+const x = parseFloat(prompt("Digite o primeiro valor: "));
+const y = parseFloat(prompt("Digte o segundo valor: "));
+
+
+alert (
+"Adição: " + (x + y) + "\n" +
+"Subtração: " + (x - y) + "\n" +
+"Divisão: " + (x / y).toFixed(2) + "\n" +
+"Multiplicação: " + (x * y)
+);
